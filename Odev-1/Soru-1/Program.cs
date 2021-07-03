d89e9c0db27f3a62d145759049935c3c89baa8af
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-07-03 19:13:42 +0300

    C#-101 Odev 1 algoritma soruları

---
+++ Odev-1/Soru-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Soru_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // do
+            // {
+            //     Console.Write("Dizi boyutunu giriniz:");
+            //     boyut = int.Parse(Console.ReadLine());
+
+            // } while (boyut <= 0);
+
+            Console.Write("Kaç elemanlı olsun:");
+            int boyut = int.Parse(Console.ReadLine());
+
+            int[] sayiDizisi = new int[boyut];
+            for (int i = 0; i < boyut; i++)
+            {
+                Console.Write($"{i + 1}. sayıyı giriniz:");
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Girilen sayıların çift olanları");
+            foreach (var item in sayiDizisi)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
---
+++ Odev-1/Soru-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Soru_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dizi boyutunu giriniz:");
+            int arrLength = int.Parse(Console.ReadLine());
+
+            Console.Write("Sayıları bölmesini istediğiniz sayıyı giriniz:");
+            int divider = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[arrLength];
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write($"{i + 1}. sayıyı giriniz:");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Girilen sayıların, {divider} ile tam bölünen ya da eşit olanları");
+            foreach (var item in arr)
+            {
+                if (item % divider == 0 || item == divider)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
