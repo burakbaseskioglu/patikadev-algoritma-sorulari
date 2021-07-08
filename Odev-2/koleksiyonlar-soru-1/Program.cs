868c0fec87e9705af07b2f6e47fa044acf48e147
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-07-09 01:52:26 +0300

    patika.dev c# 101 2. ödev soru 1 ve 3

---
+++ Odev-2/koleksiyonlar-soru-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace koleksiyonlar_soru_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList asalOlanlar = new ArrayList();
+            ArrayList asalOlmayanlar = new ArrayList();
+            ArrayList sayilar = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{i + 1}. sayyıyı giriniz:");
+                int sayi = int.Parse(Console.ReadLine());
+                sayilar.Add(sayi);
+            }
+            int sayac = 0;
+            foreach (var item in sayilar)
+            {
+                for (int i = 2; i < (int)item; i++)
+                {
+                    if ((int)item % i == 0)
+                    {
+                        sayac++;
+                        break;
+                    }
+                }
+                if (sayac != 0)
+                {
+                    asalOlmayanlar.Add(item);
+                    sayac = 0;
+                }
+                else
+                {
+                    asalOlanlar.Add(item);
+                }
+            }
+
+            Console.WriteLine("**** Asal olanlar ****");
+            foreach (var item in asalOlanlar)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("**** Asal olmayanlar ****");
+            foreach (var item in asalOlmayanlar)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Odev-2/koleksiyonlar-soru-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace koleksiyonlar_soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            Console.Write("Cümle giriniz:");
+            string expression = Console.ReadLine();
+
+            ArrayList vowelsSentence = new ArrayList();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (expression[i] == vowels[j] && !vowelsSentence.Contains(expression[i]))
+                    {
+                        vowelsSentence.Add(expression[i]);
+                    }
+                }
+            }
+            
+            Console.WriteLine("**** Cümledeki sesli harfler ****");
+            vowelsSentence.Sort();
+            foreach (var item in vowelsSentence)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
